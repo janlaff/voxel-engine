e1846441355052c1552a4a20f27669a16ed8ca9b
jan <jan@lafferton.de>
2023-03-29 18:46:45 +0200

    refactor stuff

--- voxel-engine-gpu/src/octree/mod.rs
+++
@@ -1,5 +0,0 @@
-mod node;
-mod traversal;
-
-pub use node::*;
-pub use traversal::*;
--- voxel-engine-gpu/src/sky.rs
+++
@@ -1,7 +0,0 @@
-use crate::ray::Ray;
-use glam::Vec3;
-
-pub fn sky_color(ray: &Ray) -> Vec3 {
-    let t = 0.5 * (ray.direction.y + 1.0);
-    return (1.0 - t) * Vec3::splat(1.0) + t * Vec3::new(0.5, 0.7, 1.0);
-}
